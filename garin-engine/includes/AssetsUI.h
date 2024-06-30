a551a6b71d8c2265e8535835227436d4e64e02ee
Esteban Nova <85538374+GrstudiosGamesPro@users.noreply.github.com>
2024-06-30 01:47:40 -0500

    NEW DATA
    - Drags files (FBX, OBJ, VS, FS, GLSL, MTL, MATERIAL, WAV, MP3, PNG, JPG)

--- garin-engine/includes/AssetsUI.h
+++ garin-engine/includes/AssetsUI.h
@@ -10,71 +10,7 @@ public:
     std::string selected_item;
 
     void draw(EditorConfigs *p_configs) override;
-    void ShowDirectoryTree(const std::filesystem::path &path)
-    {
-        static double last_click_time = -1.0;
-        constexpr double double_click_time = 0.2;
-
-        for (const auto &entry : std::filesystem::directory_iterator(path))
-        {
-            if (entry.is_directory())
-            {
-                if (ImGui::TreeNode(entry.path().filename().string().c_str()))
-                {
-                    if (ImGui::IsItemClicked(0))
-                    {
-                        double current_time = ImGui::GetTime();
-                        if (current_time - last_click_time <= double_click_time)
-                        {
-                            // Doble clic detectado
-                            selected_item = entry.path().string();
-                            std::filesystem::path selected_path(selected_item);
-                            std::string extension = selected_path.extension().string();
-                            std::string base_name = selected_path.stem().string();
-
-                            if (extension == ".scene")
-                            {
-                                Graphics::get_current_scene()->load_scene(base_name);
-                            }
-                            std::cout << "Directorio doble clic: " << selected_item << std::endl;
-                        }
-                        else
-                        {
-                            // Solo primer clic
-                            last_click_time = current_time;
-                        }
-                    }
-
-                    ShowDirectoryTree(entry.path());
-                    ImGui::TreePop();
-                }
-            }
-            else
-            {
-                if (ImGui::Selectable(entry.path().filename().string().c_str(), selected_item == entry.path().string()))
-                {
-                    double current_time = ImGui::GetTime();
-                    if (current_time - last_click_time <= double_click_time)
-                    {
-                        // Doble clic detectado
-                        selected_item = entry.path().string();
-                        std::filesystem::path selected_path(selected_item);
-                        std::string extension = selected_path.extension().string();
-                        std::string base_name = selected_path.stem().string();
-
-                        if (extension == ".scene")
-                        {
-                            Graphics::get_current_scene()->load_scene(base_name);
-                        }
-                        std::cout << "Archivo doble clic: " << selected_item << std::endl;
-                    }
-                    else
-                    {
-                        // Solo primer clic
-                        last_click_time = current_time;
-                    }
-                }
-            }
-        }
-    }
+    void ShowDirectoryTree(const std::filesystem::path &path);
+    void drawer_files(std::string extension, std::string filename);
+    void drawer_files_drag(std::string extension, std::string file_name);
 };
